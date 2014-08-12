2650f372c4754af15acb97bf6614231005a4ff50
Robert Jackson <robert.w.jackson@me.com>
2014-08-11 21:19:46 -0400

    Initial commit.

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  name: 'live-reload-middleware',
+
+  serverMiddleware: function(options) {
+    var app = options.app;
+    options = options.options;
+
+    if (options.liveReload === true) {
+      var livereloadMiddleware = require('connect-livereload');
+
+      app.use(livereloadMiddleware({
+        port: options.loveReloadPort
+      }));
+    }
+  }
+};
