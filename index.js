ca11170f65c29fb15615b655d7a9d8c506086a59
Jean-Baptiste Escoyez <jbescoyez@gmail.com>
2015-11-19 12:32:13 +0100

    Make `EMBER_CLI_INJECT_LIVE_RELOAD_BASEURL` configurable

--- index.js
+++ index.js
@@ -32,9 +32,7 @@ module.exports = {
     if (options.liveReload !== true) { return; }
 
     process.env.EMBER_CLI_INJECT_LIVE_RELOAD_PORT = options.liveReloadPort;
-    if (process.env.EMBER_CLI_INJECT_LIVE_RELOAD_BASEURL === undefined) {
-      process.env.EMBER_CLI_INJECT_LIVE_RELOAD_BASEURL = options.baseURL; // default is '/'  
-    }
+    process.env.EMBER_CLI_INJECT_LIVE_RELOAD_BASEURL = options.liveReloadBaseUrl || options.baseURL;
 
     app.use(options.baseURL + 'ember-cli-live-reload.js', function(request, response, next) {
       response.contentType('text/javascript');
