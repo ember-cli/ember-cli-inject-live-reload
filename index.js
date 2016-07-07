bd048864cb5952c38066d8f200237f7e716a11ac
Daniel Kmak <kuzirashi@interia.pl>
2016-07-07 13:25:16 +0200

    Make host configurable.

--- index.js
+++ index.js
@@ -14,9 +14,15 @@ module.exports = {
 
   dynamicScript: function(request) {
     var liveReloadPort = process.env.EMBER_CLI_INJECT_LIVE_RELOAD_PORT;
+    var liveReloadHostURL = process.env.EMBER_CLI_LIVE_RELOAD_HOSTURL;
+    var host = "' + (location.hostname || 'localhost') + '"; // JS code to figure out correct host
+
+    if (liveReloadHostURL) {
+      host = liveReloadHostURL; // append static string to script if option defined instead
+    }
 
     return "(function() {\n " +
-           "var src = (location.protocol || 'http:') + '//' + (location.hostname || 'localhost') + ':" + liveReloadPort + "/livereload.js?snipver=1';\n " +
+           "var src = (location.protocol || 'http:') + '//" + host + ":" + liveReloadPort + "/livereload.js?snipver=1';\n " +
            "var script    = document.createElement('script');\n " +
            "script.type   = 'text/javascript';\n " +
            "script.src    = src;\n " +
@@ -33,6 +39,7 @@ module.exports = {
 
     process.env.EMBER_CLI_INJECT_LIVE_RELOAD_PORT = options.liveReloadPort;
     process.env.EMBER_CLI_INJECT_LIVE_RELOAD_BASEURL = options.liveReloadBaseUrl || options.baseURL;
+    process.env.EMBER_CLI_LIVE_RELOAD_HOSTURL = options.liveReloadHostUrl;
 
     app.use(options.baseURL + 'ember-cli-live-reload.js', function(request, response, next) {
       response.contentType('text/javascript');
