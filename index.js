6fcf0acc62c6ef1e9eabe7d98b94f151a7a2a182
Jakub Gocławski <gocek13@gmail.com>
2016-07-18 18:36:55 +0200

    Refactor.

--- index.js
+++ index.js
@@ -28,11 +28,12 @@ module.exports = {
     var self = this;
     var app = config.app;
     var options = config.options;
+    var baseURL = options.liveReloadBaseUrl || options.baseURL;
 
     if (options.liveReload !== true) { return; }
 
     process.env.EMBER_CLI_INJECT_LIVE_RELOAD_PORT = options.liveReloadPort;
-    process.env.EMBER_CLI_INJECT_LIVE_RELOAD_BASEURL = options.liveReloadBaseUrl || options.baseURL;
+    process.env.EMBER_CLI_INJECT_LIVE_RELOAD_BASEURL = baseURL;
 
     app.use(options.baseURL + 'ember-cli-live-reload.js', function(request, response, next) {
       response.contentType('text/javascript');
