37548a277b9cc95a8d2e12244830c640c6d076b5
Jean-Baptiste Escoyez <jbescoyez@gmail.com>
2016-11-14 21:06:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -34,12 +34,12 @@ module.exports = {
     var app = config.app;
     var options = config.options;
     // maintaining `baseURL` for backwards compatibility. See: http://emberjs.com/blog/2016/04/28/baseURL.html
-    var baseURL = options.liveReloadBaseUrl || options.rootURL || options.baseURL;
+    var baseURL = options.rootURL || options.baseURL;
 
     if (options.liveReload !== true) { return; }
 
     process.env.EMBER_CLI_INJECT_LIVE_RELOAD_PORT = options.liveReloadPort;
-    process.env.EMBER_CLI_INJECT_LIVE_RELOAD_BASEURL = baseURL;
+    process.env.EMBER_CLI_INJECT_LIVE_RELOAD_BASEURL = options.liveReloadBaseUrl || baseURL;
 
     app.use(baseURL + 'ember-cli-live-reload.js', function(request, response, next) {
       response.contentType('text/javascript');
